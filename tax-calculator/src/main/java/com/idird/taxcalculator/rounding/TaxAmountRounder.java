d582fa710f4edd91637a3c8f65d9757cd778fa8d
idah <idir.dahmouh@gmail.com>
2018-05-21 10:03:04 +0200

    Rename RoundingRate on RoundingMt

--- tax-calculator/src/main/java/com/idird/taxcalculator/rounding/TaxAmountRounder.java
+++ tax-calculator/src/main/java/com/idird/taxcalculator/rounding/TaxAmountRounder.java
@@ -8,21 +8,21 @@ import java.math.RoundingMode;
 public class TaxAmountRounder {
 
     private int scale;
-    private BigDecimal roundingRate;
+    private BigDecimal roundingMt;
     private RoundingMode roundingMode;
 
-    public TaxAmountRounder(final int p_scale, final BigDecimal p_roundingRate, final RoundingMode p_roundingMode) {
+    public TaxAmountRounder(final int p_scale, final BigDecimal p_roundingMt, final RoundingMode p_roundingMode) {
         this.scale = p_scale;
-        this.roundingRate = p_roundingRate;
+        this.roundingMt = p_roundingMt;
         this.roundingMode = p_roundingMode;
     }
 
     public BigDecimal round(BigDecimal p_amount) {
-        if (roundingRate.signum() == 0) {
+        if (roundingMt.signum() == 0) {
             return p_amount;
         }
 
-        BigDecimal percentage = roundingRate.divide(DefaultConstants.ONE_HUNDRED);
+        BigDecimal percentage = roundingMt.divide(DefaultConstants.ONE_HUNDRED);
         BigDecimal ratio = p_amount.divide(percentage).setScale(0, roundingMode);
         BigDecimal result = ratio.multiply(percentage);
         return result.setScale(scale);
